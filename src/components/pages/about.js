0aba615d5003b81d21be0f3147779e930d6229f3
AmberlynSisk <amberlynsisk@yahoo.com>
2022-03-15 00:51:31 -0500

    finishing styles on portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import profilePicture from '../../../static/assets/images/bio/bioPic.jpg';
+
 
 export default function() {
     return (
-        <div>
-            About
+        <div className="content-page-wrapper">
+            <div className="left-column" 
+            style={{
+                background: "url(" + profilePicture + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+            }}
+            />
+            <div className="right-column">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum totam commodi natus aut debitis excepturi in exercitationem, dolorem cupiditate expedita nemo, repellat asperiores architecto animi quaerat quo tempore, rem non.
+                Dolorum, consequatur doloremque? Ducimus asperiores repellat incidunt ex voluptates quos dolorum eveniet, reprehenderit nulla culpa est quod rem. Dolorum harum ducimus, quam consequatur ipsam beatae vitae nihil totam consequuntur. Nam?
+                Dolorem, accusantium aut vero numquam consequatur facere accusamus? Saepe temporibus repudiandae nulla, eius ullam delectus, suscipit deleniti cupiditate unde natus doloribus ad nisi odio, alias voluptates culpa a minus placeat!
+                Cumque fugiat expedita, adipisci ipsa sed nisi? Mollitia, cupiditate dolorum? Delectus incidunt, velit officiis consequuntur harum a nemo rerum inventore nesciunt et tenetur nisi nihil, est impedit labore maxime beatae!
+            </div>
         </div>
     );
 }
